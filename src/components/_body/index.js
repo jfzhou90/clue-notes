40dffcd871e7e49ab31857f32713958dd2422e0c
Joey Zhou <jfzhou.nc@gmail.com>
2019-01-19 16:51:22 -0500

    add 'add player' button

--- src/components/_body/index.js
+++ src/components/_body/index.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
-import CheckBox from './_checkbox.js';
+// import CheckBox from './_checkbox.js';
 
 class Body extends Component{
+  constructor(){
+    super()
+    this.state = {
+      players: ['You']
+    }
+
+  };
+
+  player_cards = ['Professor Plum', 'Mr.Green'];
+  weapon_cards = ['Knife', 'Gun'];
+
+  async addPlayer(){
+    let player = window.prompt("What's the name of the player?");
+    await this.setState({players: [...this.state.players, player]});
+  };
+
   render(){
     return (
       <div className="Container-row Body">
-        <CheckBox/>
+        { this.state.players.length < 6 &&
+          <button className="Body-button-add_player" onClick={()=>this.addPlayer()}>Add Player</button>
+        }
       </div>
     )};
 };
