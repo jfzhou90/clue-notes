89521c431e158a4f460d3bed34ddd771fbd5afe2
Joey Zhou <jfzhou.nc@gmail.com>
2019-01-19 14:21:54 -0500

    add header

---
+++ src/components/_body/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+
+class Header extends Component{
+  render(){
+    return (
+      <div className="Container-row Header">
+          <img src={logo} className="Header-logo Flex-grow-1" alt="logo" />
+          <span className='Header-title Flex-grow-2'>Detective's Notes</span>
+      </div>
+    )};
+};
+
+export default Header;
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+// This serves as a import export index file for all components.
+// so when needed, all you have to do is import {component} from './components'.
+import Header from './_header.js';
+
+export {
+  Header
+};
